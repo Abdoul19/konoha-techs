07cfcf30b975ea55f3b572937305d49c5c090b97
Abdoul19 <abdoul19@gmail.com>
2020-11-30 18:44:47 +0000

    Init

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+    <v-app>
+      <v-app-bar
+        app
+        color="transparent"
+        flat
+      >
+        <div class="d-flex align-center">
+          <v-img
+            alt="Konoha Logo"
+            class="shrink mr-2"
+            contain
+            src="./assets/logo/web/logo-web-transparent.png"
+            transition="scale-transition"
+            width="200"
+          />
+        </div>
+        <v-spacer></v-spacer>
+
+        <v-btn
+          href=""
+          target="_blank"
+          text
+        >
+          <span class="mr-2">Home</span>
+        </v-btn>
+
+        <v-btn
+          href=""
+          target="_blank"
+          text
+        >
+          <span class="mr-2">Services</span>
+        </v-btn>
+
+        <v-btn
+          href=""
+          target="_blank"
+          text
+        >
+          <span class="mr-2">About Us</span>
+        </v-btn>
+
+        <v-btn
+          href=""
+          target="_blank"
+          text
+        >
+          <span class="mr-2">Contacts</span>
+        </v-btn>
+      </v-app-bar>
+
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </v-app>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-row>    
+      <v-card>
+        <v-img height="100%" width="100%" src="../assets/hero-background.jpg" />
+      </v-card>
+    </v-row>
+  
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    //HelloWorld
+  }
+}
+</script>
