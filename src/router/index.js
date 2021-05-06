c2a5b5f5d2ac3209ab5f365d31b89cac3c6a7543
Abdoul19 <abdoul19@gmail.com>
2021-05-06 16:57:51 +0000

    Page meta

--- src/router/index.js
+++ src/router/index.js
@@ -10,37 +10,128 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: 'Home - Konoha Technologies',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The home page of Konoha Technologies.'
+        },
+        {
+          property: 'og:description',
+          content: 'The home page of Konoha Technologies.'
+        }
+      ]
+    }
   },
   {
     path: '/services',
     name: 'Service',
-    component: () => import(/* webpackChunkName: "service" */ '../views/Service.vue')
+    component: () => import(/* webpackChunkName: "service" */ '../views/Service.vue'),
+    meta: {
+      title: 'Services - Konoha Technologies',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The service page of Konoha Technologies.'
+        },
+        {
+          property: 'og:description',
+          content: 'The service page of Konoha Technologies.'
+        }
+      ]
+    }
   },
   {
     path: '/services/app-modernization',
     name: 'ServiceAppModerniz',
-    component: () => import(/* webpackChunkName: "ServiceAppModerniz" */ '../components/Services/AppModerniz.vue')
+    component: () => import(/* webpackChunkName: "ServiceAppModerniz" */ '../components/Services/AppModerniz.vue'),
+    meta: {
+      title: 'App Modernization - Konoha Technologies',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The service app modernization page of Konoha Technologies.'
+        },
+        {
+          property: 'og:description',
+          content: 'The service app modernization page of Konoha Technologies.'
+        }
+      ]
+    }
   },
   {
     path: '/services/cloud',
     name: 'ServiceCloud',
-    component: () => import(/* webpackChunkName: "ServiceCloud" */ '../components/Services/Cloud.vue')
+    component: () => import(/* webpackChunkName: "ServiceCloud" */ '../components/Services/Cloud.vue'),
+    meta: {
+      title: 'Cloud - Konoha Technologies',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The service cloud page of Konoha Technologies.'
+        },
+        {
+          property: 'og:description',
+          content: 'The service cloud page of Konoha Technologies.'
+        }
+      ]
+    }
   },
   {
     path: '/services/cto',
     name: 'ServiceCto',
-    component: () => import(/* webpackChunkName: "ServiceCto" */ '../components/Services/Cto.vue')
+    component: () => import(/* webpackChunkName: "ServiceCto" */ '../components/Services/Cto.vue'),
+    meta: {
+      title: 'Chief Technical Operator - Konoha Technologies',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The service chief technical operator page of Konoha Technologies.'
+        },
+        {
+          property: 'og:description',
+          content: 'The service chief technical operator page of Konoha Technologies.'
+        }
+      ]
+    }
   },
   {
     path: '/services/dev-process',
     name: 'ServiceDevProcess',
-    component: () => import(/* webpackChunkName: "ServiceDevProcess" */ '../components/Services/DevProcess.vue')
+    component: () => import(/* webpackChunkName: "ServiceDevProcess" */ '../components/Services/DevProcess.vue'),
+    meta: {
+      title: 'Developement Process - Konoha Technologies',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The service developement process consulting page of Konoha Technologies.'
+        },
+        {
+          property: 'og:description',
+          content: 'The service developement process consulting page of Konoha Technologies.'
+        }
+      ]
+    }
   },
   {
     path: '/services/web-dev',
     name: 'ServiceWebDev',
-    component: () => import(/* webpackChunkName: "ServiceWebDev" */ '../components/Services/WebDev.vue')
+    component: () => import(/* webpackChunkName: "ServiceWebDev" */ '../components/Services/WebDev.vue'),
+    meta: {
+      title: 'Web Developpement - Konoha Technologies',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The service web and mobile developement page of Konoha Technologies.'
+        },
+        {
+          property: 'og:description',
+          content: 'The service web and mobile developement page of Konoha Technologies.'
+        }
+      ]
+    }
   },
   {
     path: '/about',
@@ -48,7 +139,20 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: {
+      title: 'About Us - Konoha Technologies',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The about us page of Konoha Technologies.'
+        },
+        {
+          property: 'og:description',
+          content: 'The about us page of Konoha Technologies.'
+        }
+      ]
+    }
   },
   {
     path: '/contacts',
@@ -56,7 +160,20 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "Contact" */ '../views/Contact.vue')
+    component: () => import(/* webpackChunkName: "Contact" */ '../views/Contact.vue'),
+    meta: {
+      title: 'Contact - Konoha Technologies',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The contact page of Konoha Technologies.'
+        },
+        {
+          property: 'og:description',
+          content: 'The contact page of Konoha Technologies.'
+        }
+      ]
+    }
   }
 ]
 
@@ -64,5 +181,49 @@ const router = new VueRouter({
   mode: 'history',
   routes
 })
+
+// This callback runs before every route change, including on page load.
+router.beforeEach((to, from, next) => {
+  // This goes through the matched routes from last to first, finding the closest route with a title.
+  // e.g., if we have `/some/deep/nested/route` and `/some`, `/deep`, and `/nested` have titles,
+  // `/nested`'s will be chosen.
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
+
+  // Find the nearest route element with meta tags.
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+
+  const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+
+  // If a route with a title was found, set the document (page) title to that value.
+  if (nearestWithTitle) {
+    document.title = nearestWithTitle.meta.title;
+  } else if (previousNearestWithMeta) {
+    document.title = previousNearestWithMeta.meta.title;
+  }
+
+  // Remove any stale meta tags from the document using the key attribute we set below.
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
+
+  // Skip rendering meta tags if there are none.
+  if (!nearestWithMeta) return next();
+
+  // Turn the meta tag definitions into actual elements in the head.
+  nearestWithMeta.meta.metaTags.map(tagDef => {
+    const tag = document.createElement('meta');
+
+    Object.keys(tagDef).forEach(key => {
+      tag.setAttribute(key, tagDef[key]);
+    });
+
+    // We use this to track which meta tags we create so we don't interfere with other ones.
+    tag.setAttribute('data-vue-router-controlled', '');
+
+    return tag;
+  })
+    // Add the meta tags to the document head.
+    .forEach(tag => document.head.appendChild(tag));
+
+  next();
+});
 
 export default router
