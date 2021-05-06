280255406e0a55f8cb4d0094778187b5614ef5bc
Abdoul19 <abdoul19@gmail.com>
2021-05-06 17:24:41 +0000

    update home hero img

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <v-container pa-0 fluid>
-        <v-parallax height="300" width="100%" src="../assets/hero-background.jpg">
+        <v-parallax height="300" width="100%" src="../assets/hero-background.webp">
           <v-row  align="center" justify="center" >
             <v-col class="text-center" cols="12" md=6>
             <h1 class="font-weight-regular font-weight-sm-black primary--text text--lighten-1">
